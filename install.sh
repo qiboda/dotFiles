3535904d95b736fff222bf0119adcdb0f5558edb
qiboda <qiboda00@gmail.com>
2020-03-21 15:34:27 +0800

    add polybar

--- install.sh
+++ install.sh
@@ -11,12 +11,19 @@ sudo pacman -Syu feh ripgrep rsync fzf thefuck htop trash-cli cloc aria2 shellch
     mandb \
     cmake \
     clang \
+    dhcpcd \
 
 # yay
 yay -Syu oh-my-zsh-git zsh-autosuggestions autojump
 
 # python
 sudo pip install cheat
+
+###############################################
+# gui
+###############################################
+
+yay -Syu i3-gnome polybar rofi networkmanager gdm
 
 ###############################################
 # neovim
